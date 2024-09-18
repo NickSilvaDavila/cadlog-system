46e1651edb105e861943b2681543374b36fa0877
NickSilvaDavila <nicolly9620@gmail.com.br>
2024-09-18 08:32:50 -0300

    Atualizar alteraçoes no codigo user.php.

--- models/user.php
+++ models/user.php
@@ -1,6 +1,24 @@
 <?php
 
 // Inclui o arquivo de conexao que contem a classe DATABASE  para gerenciar a conexao com o BD
+
 require_once 'models/database.php';
+
+class User{
+    // Funçao para encontrar um usuario pelo email
+    public static function findByEmail($email){
+
+        // Coleta a conexao com o BD
+        $conn = Database::getConnection();
+
+
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+     // executa  a consulta com o e-mail passadocomo parametro
+        $stmt->execute(['email' => $email]);
+
+       // Retorna os dados  do usuario encontrado como um array associativo 
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
 
 ?>
