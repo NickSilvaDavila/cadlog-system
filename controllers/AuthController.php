ca4ef7c3d90d545a54489ce2104418a6f5dbfa88
NickSilvaDavila <nicolly9620@gmail.com.br>
2024-09-18 08:04:39 -0300

    Atualizar o arquivo.

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,5 +1,20 @@
 <?php
 
-require 'models/user.php';
+require_once 'models/user.php';
+
+// classe responsavel pela autentificaçao do usuario
+class AuthController
+{
+    // Funçao responsavel pelo processo de login 
+    public function login(){
+
+        // Verifica se a requisiçao HTTP e do tipo POST, ou seja, se o formulario foi enviado
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+        }
+    }
+}
 
 ?>
