7e5bcbef2330adfc22d0f7b0540f93cb9ac38f53
NickSilvaDavila <nicolly9620@gmail.com.br>
2024-10-23 10:13:26 -0300

    Define as rotas da aplicação, mapeando URLs para controladores específicos. Inclui as definições de rotas para o cadastro de usuários, login e listagem de usuários. Facilita a navegação entre diferentes partes da aplicação.

--- routes.php
+++ routes.php
@@ -6,11 +6,11 @@ require 'controllers/DashboardController.php'; // Instancia o controlador de das
 
 // Cria instâncias dos controladores para utilizar seus métodos
 $authController = new AuthController();
-$userController = new UserController(); // Corrigido o nome da variável
-$dashboardController = new DashboardController(); // Corrigido o nome da variável
+$userController = new UserController(); 
+$dashboardController = new DashboardController(); 
 
 // Coleta a ação da URL, se não houver ação definida, usa 'login' como padrão
-$action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+$action = $_GET['action'] ?? 'login'; 
 
 switch ($action) {
     case 'login':
@@ -22,11 +22,15 @@ switch ($action) {
         break;
 
     case 'register':
-        $userController->register(); // Corrigido o nome da variável
+        $userController->register();
         break;
 
     case 'dashboard':
-        $dashboardController->index(); // Corrigido o nome da variável
+        $dashboardController->index();
+        break;
+        
+    case 'list':
+        $userController->list(); // Especifique o nome correto do método aqui
         break;
 
     default:
